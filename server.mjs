18b30312c3f983df6434cdd384aee2b346083dd6
abubakarafridi <abubakarafridi407@gmail.com>
2024-10-17 16:07:40 +0500

    using env for data protection

--- server.mjs
+++ server.mjs
@@ -1,12 +1,15 @@
 import express from "express"
 import cors from "cors"
 import mongoose from "mongoose"
+import dotenv from "dotenv"
+
+dotenv.config();
 
 const app = express();
 app.use(cors())
 app.use(express.json())
 
-const mongoUri = "mongodb://localhost:27017/user_management";
+const mongoUri = process.env.MONGODB_LOCAL_URI;
 
 mongoose.connect(mongoUri, {
     useNewUrlParser: true,
