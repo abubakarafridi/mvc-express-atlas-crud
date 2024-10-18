9a2d27b5127877fbb117871a7feedf217eced095
abubakarafridi <abubakarafridi407@gmail.com>
2024-10-18 15:42:23 +0500

    UserRouter is created to manage the different routes and methods

---
+++ routers/userRoutes.mjs
@@ -0,0 +1,16 @@
+import express from 'express';
+import {
+  addUser,
+  deleteUser,
+  getAllUsers,
+  updatedUser,
+} from '../controllers/UserController.mjs';
+
+const router = express.Router();
+
+router.get('users', getAllUsers);
+router.post('users', addUser);
+router.put('users/:id', updatedUser);
+router.delete('users/:id', deleteUser);
+
+export default router
